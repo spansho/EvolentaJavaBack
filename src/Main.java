fcc3143bbe265f84c5c013aa8521bbbff4f580c3
Egor <egordedikovrvdkr@mail.ru>
2025-02-27 21:11:51 +0300

    Выполнение задания 2.4.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
-        Scanner scanner = new Scanner(System.in);
-        String inputString=scanner.nextLine();
-        System.out.println(String.format("Здраствуй, %s.",inputString));
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Введите строку");
+        String string=scanner.nextLine();
+        System.out.println("Введите подстроку");
+        String subString=scanner.nextLine();
+        int countOfEntryOfSubstring=(string.length() - string.replace(subString, "").length()) / subString.length();
+        System.out.println(String.format("Подстрока %s входит в строку %d раз",subString,countOfEntryOfSubstring));
     }
 }
