9c014cd581e2c4fe41c0d9156138c61c407e2e9f
Egor <egordedikovrvdkr@mail.ru>
2025-02-27 21:54:45 +0300

    Выполнение задания 2.4.4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.text.ParseException;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Введите строку");
+        System.out.println("Введите дату в формате 'дд.мм.гггг");
         String string=scanner.nextLine();
-        System.out.println("Введите подстроку");
-        String subString=scanner.nextLine();
-        int countOfEntryOfSubstring=(string.length() - string.replace(subString, "").length()) / subString.length();
-        System.out.println(String.format("Подстрока %s входит в строку %d раз",subString,countOfEntryOfSubstring));
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        // Формат целевой даты
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = inputFormat.parse(string);
+
+            // Преобразуем объект Date в строку с новым форматом
+            String outputDate = outputFormat.format(date);
+            System.out.println(outputDate);
+        } catch (ParseException e) {
+            System.out.println("Ошибка при парсинге даты: " + e.getMessage());
+        }
     }
 }
