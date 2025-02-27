921c76059632e053c7c8d73709ad79a9c81e9aa4
Egor <egordedikovrvdkr@mail.ru>
2025-02-27 18:24:33 +0300

    Выполнение задания 2.2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
+        System.out.println("Введите числа a b и c");
         Scanner scanner = new Scanner(System.in);
-        String inputString=scanner.nextLine();
-        System.out.println(String.format("Здраствуй, %s.",inputString));
+        int a,b,c;
+        a= scanner.nextInt();
+        b=scanner.nextInt();
+        c=scanner.nextInt();
+        if(a%5==0) {System.out.println("Число a делится на 5");System.out.println(a);System.out.println();}
+        if(b%5==0) {System.out.println("Число b делится на 5");System.out.println(b);System.out.println();}
+        if(c%5==0) {System.out.println("Число c делится на 5");System.out.println(c);System.out.println();}
+        System.out.println("Результат целочисленного деления a на b");
+        System.out.println(a/b);
+        System.out.println();
+        System.out.println("Результат вещественного деления a на b");
+        System.out.println((double)a/b);
+        System.out.println();
+        System.out.println("Результат округления деления a на b в большую сторону");
+        System.out.println(Math.ceil((double)a/b));
+        System.out.println();
+        System.out.println("Результат округления деления a на b в меньшую сторону");
+        System.out.println(Math.floor((double)a/b));
+        System.out.println();
+        System.out.println("Результат математического округления  деления a на b ");
+        System.out.println(Math.round(a/b));
+        System.out.println();
+        System.out.println("Остаток от деления b на c ");
+        System.out.println(b%c);
+        System.out.println();
+        System.out.println("Наименьшее среди a и b");
+        System.out.println(Math.min(a,b));
+        System.out.println();
+        System.out.println("Наименьшее среди b и c");
+        System.out.println(Math.max(b,c));
     }
 }
