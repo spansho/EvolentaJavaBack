dcfe006602169395f8c216177136aec31bcd7d19
Egor <egordedikovrvdkr@mail.ru>
2025-02-28 09:54:31 +0300

    исправление task2.4

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         System.out.println("Введите дату в формате 'дд.мм.гггг");
-        String string=scanner.nextLine();
+        String stringa=scanner.nextLine();
+        String newString=stringa.substring(stringa.length()-4,stringa.length())+"-"+stringa.substring(3,5)+"-"+stringa.substring(0,2);
+
+        System.out.println("Преоразование с помощью методов строки");
+        System.out.println(newString);
+
         SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM.yyyy");
 
-        // Формат целевой даты
         SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Преоразование с помощью Date");
         try {
-            Date date = inputFormat.parse(string);
+            Date date = inputFormat.parse(stringa);
 
-            // Преобразуем объект Date в строку с новым форматом
             String outputDate = outputFormat.format(date);
             System.out.println(outputDate);
         } catch (ParseException e) {
