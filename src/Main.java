b2632fffaa20159299d9a9eaabcacce390137c2a
Egor <egordedikovrvdkr@mail.ru>
2025-02-28 21:49:02 +0300

    Выполение task3.1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
-        Scanner scanner = new Scanner(System.in);
-        String inputString=scanner.nextLine();
-        System.out.println(String.format("Здраствуй, %s.",inputString));
+        Scanner scanner=new Scanner(System.in);
+        String name;
+       int age;
+       System.out.println("Введите имя");
+       name=scanner.nextLine();
+       System.out.println("Введите возраст");
+       age=scanner.nextInt();
+       User user=new User(name,age);
+
+        scanner.nextLine();
+        System.out.println("Введите имя");
+        name=scanner.nextLine();
+        System.out.println("Введите возраст");
+        age=scanner.nextInt();
+        User user2=new User(name,age);
+        System.out.println(" ");
+        System.out.println(user.toString());
+        System.out.println(user2.toString());
+
+
     }
 }
+
+
---
+++ src/User.java
@@ -0,0 +1,39 @@
+public class User {
+    private String name;
+    private Integer age;
+
+
+
+    public User()
+    {
+        setName("no name");
+        setAge(0);
+    }
+
+    public User(String namez,Integer agez)
+    {
+        setName(namez);
+        setAge(agez);
+    }
+    public String toString() {
+        String newString=name+" возраст"+age;
+        return newString;
+    }
+
+
+    public String getName() {
+        return  name;
+    }
+
+    public void setName (String namez)
+    {
+        name = namez;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
+
+    public void setAge(Integer agez){age=agez;};
+
+}
