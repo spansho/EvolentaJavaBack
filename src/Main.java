5be010c4461fa4800bb6d929d12d14dde36d5a7b
Egor <egordedikovrvdkr@mail.ru>
2025-03-03 12:03:13 +0300

    Выполение task4.2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
-        Scanner scanner = new Scanner(System.in);
-        String inputString=scanner.nextLine();
-        System.out.println(String.format("Здраствуй, %s.",inputString));
+
+        long sum =getArithmeticProgressionSum(10000000,1000000000);
+        System.out.println(sum);
+    }
+    public static long getArithmeticProgressionSum(int a,int b)
+    {
+        LongStream stream=LongStream.range(a,b);
+        return stream.reduce(0L,(acc,n)->{
+            if(n!=b)
+                acc+=n;
+            return acc;
+        });
     }
 }
