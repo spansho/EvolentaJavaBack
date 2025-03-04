8eb03d084efa918c097c63cf852c0b9196b9dc52
Egor <egordedikovrvdkr@mail.ru>
2025-03-04 13:36:32 +0300

    Исправление task4.4///

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,10 +8,16 @@ public class Main {
         Scanner scanner= new Scanner(System.in);
         Calculator calculatorMultiplie=new Calculator(new Operations.Multiplie());
         Calculator calculatorDivided=new Calculator(new Operations.Divided());
+        Calculator calculatorAdder=new Calculator(new Operations.Adder());
+        Calculator calculatorSustractor=new Calculator(new Operations.Subtractor());
         System.out.println("Введите число a");
         double a=scanner.nextDouble();
         System.out.println("Введите число b");
         double b =scanner.nextDouble();
+        System.out.println("Результат операции вычитания a и b = ");
+        calculatorSustractor.calc(a,b);
+        System.out.println("Результат операции сложения a и b = ");
+        calculatorAdder.calc(a,b);
         System.out.println("Результат операции умножения a на b = ");
         calculatorMultiplie.calc(a,b);
 
